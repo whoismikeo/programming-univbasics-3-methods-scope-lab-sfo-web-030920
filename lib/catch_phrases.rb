d6ef430e841d9defdcd15ed7fad172eceb47fa1f
Michael Ochnicki <ochnicki.michael@gmail.com>
2020-02-16 03:43:36 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,8 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  phrase = "It's dangerous to go alone! Take this."
+  puts phrase
+end
